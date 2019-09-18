19874f5ffb7ab15bb8a65440b4b3f42cc9fe5702
sciencesakura <sakurasakurascience@gmail.com>
2019-09-19 00:56:40 +0900

    Refactoring

--- src/domain/sequent.ts
+++ src/domain/sequent.ts
@@ -54,7 +54,7 @@ interface DecomposeResult {
     readonly sequent2?: Sequent;
 }
 
-const result: (s1: Sequent, s2?: Sequent) => DecomposeResult = (s1: Sequent, s2?: Sequent) => {
+const _result: (s1: Sequent, s2?: Sequent) => DecomposeResult = (s1: Sequent, s2?: Sequent) => {
     return s2 ? { sequent1: _normalize(s1), sequent2: _normalize(s2) } : { sequent1: _normalize(s1) };
 };
 
@@ -68,25 +68,25 @@ const _decompose: (s: Sequent) => DecomposeResult | null = (s: Sequent) => {
             case Operator.And: {
                 excluded.push(formula.operand1!);
                 excluded.push(formula.operand2!);
-                return result(sequent(excluded, s.succedents));
+                return _result(sequent(excluded, s.succedents));
             }
             case Operator.Or: {
                 const ant2 = excluded.slice();
                 ant2.push(formula.operand2!);
                 excluded.push(formula.operand1!);
-                return result(sequent(excluded, s.succedents), sequent(ant2, s.succedents));
+                return _result(sequent(excluded, s.succedents), sequent(ant2, s.succedents));
             }
             case Operator.Imply: {
                 const suc1 = s.succedents.slice();
                 const ant2 = excluded.slice();
                 suc1.push(formula.operand1!);
                 ant2.push(formula.operand2!);
-                return result(sequent(excluded, suc1), sequent(ant2, s.succedents));
+                return _result(sequent(excluded, suc1), sequent(ant2, s.succedents));
             }
             case Operator.Not: {
                 const suc1 = s.succedents.slice();
                 suc1.push(formula.operand1!);
-                return result(sequent(excluded, suc1));
+                return _result(sequent(excluded, suc1));
             }
         }
     }
@@ -100,23 +100,23 @@ const _decompose: (s: Sequent) => DecomposeResult | null = (s: Sequent) => {
                 const suc2 = excluded.slice();
                 suc2.push(formula.operand2!);
                 excluded.push(formula.operand1!);
-                return result(sequent(s.antecedents, excluded), sequent(s.antecedents, suc2));
+                return _result(sequent(s.antecedents, excluded), sequent(s.antecedents, suc2));
             }
             case Operator.Or: {
                 excluded.push(formula.operand1!);
                 excluded.push(formula.operand2!);
-                return result(sequent(s.antecedents, excluded));
+                return _result(sequent(s.antecedents, excluded));
             }
             case Operator.Imply: {
                 const ant1 = s.antecedents.slice();
                 ant1.push(formula.operand1!);
                 excluded.push(formula.operand2!);
-                return result(sequent(ant1, excluded));
+                return _result(sequent(ant1, excluded));
             }
             case Operator.Not: {
                 const ant1 = s.antecedents.slice();
                 ant1.push(formula.operand1!);
-                return result(sequent(ant1, excluded));
+                return _result(sequent(ant1, excluded));
             }
         }
     }
